d1ea96a3775cdaa2994cb5c7a6629a6f202c8d08
WojciechGalecki <wojciech.galecki@wp.pl>
2019-12-28 20:47:38 +0100

    initial commit

---
+++ src/test/java/wg/grpc/GrpcSpringApplicationTests.java
@@ -0,0 +1,12 @@
+package wg.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
